bfd85eab04948f2de3ec8da7d7fe4f8f3c9a9943
Peter Becich <peterbecich@gmail.com>
2016-10-09 15:42:02 -0700

    ubuntu theme

--- emacs/init_ubuntu.el
+++ emacs/init_ubuntu.el
@@ -19,9 +19,29 @@
 (pdf-tools-install)
 
 
-(set-face-background hl-line-face "#303030"); 
-
 (put 'erase-buffer 'disabled nil)
 
 
 (load "~/dotfiles/emacs/init_shared.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (mccarthy)))
+ '(custom-safe-themes
+   (quote
+    ("3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" default)))
+ '(ediff-split-window-function (quote split-window-horizontally))
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
