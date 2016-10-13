a24d52fb1634125a6490d87c2de035b32aca25c2
Peter Becich <peterbecich@gmail.com>
2016-10-12 22:20:28 -0700

    some x configs

---
+++ bin/xmonad-start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#sleep 1
+#xsetroot -grey
+#nm-applet &
+#dropbox start
+#xfce4-power-manager &
+#trayer &
+#pidgin &
+#skype &
+#steam &
+#nautilus &
+#trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --widthtype percent --width 10 --transparent true --tint 0x000000 --height 21 --distance 0 --distancefrom right --monitor 0 &
+#redshift &
+#redshift-gtk &
+#emacs --daemon &
+#exec xmonad
+
