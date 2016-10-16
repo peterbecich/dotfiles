2f497b966b6f00e4d380779f42a5e334f7248d02
Peter Becich <peterbecich@gmail.com>
2016-10-16 13:37:19 -0700

    xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,28 +7,50 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Config.Desktop
 import XMonad.Util.SpawnOnce
 import XMonad.Actions.WorkspaceNames
-import XMonad.Actions.Volume
+--import XMonad.Actions.Volume
 import System.IO
 
-baseConfig = desktopConfig
+-- baseConfig = desktopConfig
 
-main = xmonad . myConfig =<< spawnPipe "xmobar"
+
+-- main = xmonad . myConfig =<< spawnPipe "xmobar"
              
-myConfig pipe = desktopConfig {modMask = controlMask .|. mod1Mask
-        , terminal = "rxvt"
-        , startupHook = startupHook baseConfig <+> spawnOnce "rxvt"
-        , logHook = myLogHook pipe
-        , manageHook = manageDocks            
+-- myConfig pipe = desktopConfig {modMask = controlMask .|. mod1Mask
+--         , terminal = "rxvt"
+--         , startupHook = startupHook baseConfig <+> spawnOnce "rxvt"
+--         , logHook = myLogHook pipe
+--         , manageHook = manageDocks
+--         , layoutHook = avoidStruts $ layoutHook defaultConfig
+--         } `additionalKeys`
+--         [ ((controlMask .|. mod1Mask, xK_m), spawn "emacsclient -c"),
+--           ((controlMask .|. mod1Mask, xK_s), spawn "systemctl suspend -i")--,
+--  --         ((mod1Mask, xK_F7), lowerVolume 4 >> return()),
+--  --         ((mod1Mask, xK_F8), raiseVolume 4 >> return())
+                                                                          
+--         ]
+
+-- myLogHook pipe = workspaceNamesPP xmobarPP {
+--                    ppOutput = hPutStrLn pipe
+--                  } >>= dynamicLogWithPP
+
+
+main :: IO ()
+main = do
+  h <- spawnPipe "xmobar"
+  xmonad $ defaultConfig {
+          modMask = controlMask .|. mod1Mask
+        , terminal = "gnome-terminal"
+        , startupHook = startupHook defaultConfig
+        , logHook = dynamicLogWithPP $ defaultPP {
+                            ppOutput = hPutStrLn h
+                                       }
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
         } `additionalKeys`
         [ ((controlMask .|. mod1Mask, xK_m), spawn "emacsclient -c"),
-          ((controlMask .|. mod1Mask, xK_s), spawn "systemctl suspend -i"),
-          ((mod1Mask, xK_F7), lowerVolume 4 >> return()),
-          ((mod1Mask, xK_F8), raiseVolume 4 >> return())
+          ((controlMask .|. mod1Mask, xK_s), spawn "systemctl suspend -i")--,
+ --         ((mod1Mask, xK_F7), lowerVolume 4 >> return()),
+ --         ((mod1Mask, xK_F8), raiseVolume 4 >> return())
                                                                           
         ]
-
-myLogHook pipe = workspaceNamesPP xmobarPP {
-                   ppOutput = hPutStrLn pipe
-                 } >>= dynamicLogWithPP
-
 
