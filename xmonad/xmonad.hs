80e0786e0983e3474762ca41e4caf6be44d18999
Peter Becich <peterbecich@gmail.com>
2016-10-15 17:09:38 -0700

    volume

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,6 +7,7 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Config.Desktop
 import XMonad.Util.SpawnOnce
 import XMonad.Actions.WorkspaceNames
+import XMonad.Actions.Volume
 import System.IO
 
 baseConfig = desktopConfig
@@ -19,11 +20,15 @@ myConfig pipe = desktopConfig {modMask = controlMask .|. mod1Mask
         , logHook = myLogHook pipe
         , manageHook = manageDocks            
         } `additionalKeys`
-        [ ((controlMask .|. mod1Mask, xK_m), spawn "emacsclient -c") ]
+        [ ((controlMask .|. mod1Mask, xK_m), spawn "emacsclient -c"),
+          ((controlMask .|. mod1Mask, xK_s), spawn "systemctl suspend -i"),
+          ((mod1Mask, xK_F7), lowerVolume 4 >> return()),
+          ((mod1Mask, xK_F8), raiseVolume 4 >> return())
+                                                                          
+        ]
 
 myLogHook pipe = workspaceNamesPP xmobarPP {
                    ppOutput = hPutStrLn pipe
                  } >>= dynamicLogWithPP
-             
 
 
