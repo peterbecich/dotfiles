2d6d7198ab902f2a2c482ccf4e573ebe323110cf
Peter Becich <peterbecich@gmail.com>
2017-02-08 15:37:43 -0800

    markdown, bash_profile

--- emacs/init_shared.el
+++ emacs/init_shared.el
@@ -34,7 +34,7 @@
 
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-live-preview-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))
@@ -287,3 +287,4 @@
 (setq ring-bell-function 'ignore)
 ;; (setq twittering-use-master-password t)
 (setq twittering-icon-mode t)
+(setq twittering-convert-fix-size "24")
