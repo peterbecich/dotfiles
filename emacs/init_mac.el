a1bc0cc69800fc60af2b1ced8fda33830ed76ece
Peter Becich <peterbecich@gmail.com>
2017-02-10 13:30:13 -0800

    highlight color

--- emacs/init_mac.el
+++ emacs/init_mac.el
@@ -151,7 +151,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(region ((t (:background "alternateSelectedControlColor")))))
 
 
 (set-face-background hl-line-face "#dbdbdb"); Same color as greyness in gtk
