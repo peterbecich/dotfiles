cc058597fe4082ed07070c8ae522cb002ce05fe7
Peter Becich <peterbecich@gmail.com>
2017-02-10 13:37:10 -0800

    query replace color

--- emacs/init_mac.el
+++ emacs/init_mac.el
@@ -151,6 +151,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(query-replace ((t (:inherit isearch :background "alternateSelectedControlColor"))))
  '(region ((t (:background "alternateSelectedControlColor")))))
 
 
