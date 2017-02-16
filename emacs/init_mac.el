500c4453c45ec28cbef00b0af3ca8b93a9369b58
Peter Becich <peterbecich@gmail.com>
2017-02-15 17:13:30 -0800

    color

--- emacs/init_mac.el
+++ emacs/init_mac.el
@@ -148,6 +148,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "#8b4500" :foreground "#8FB28F" :box (:line-width -1 :style released-button)))))
+ '(mode-line-inactive ((t (:background "#383838" :foreground "#5F7F5F" :box (:line-width -1 :style released-button)))))
  '(query-replace ((t (:inherit isearch :background "alternateSelectedControlColor"))))
  '(region ((t (:background "alternateSelectedControlColor")))))
 
