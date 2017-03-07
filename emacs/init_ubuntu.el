abceef0f964d737743bcf8a7d2204bb1dd62fd40
Peter Becich <peterbecich@gmail.com>
2017-03-06 16:11:57 -0800

    xmonad fixes

--- emacs/init_ubuntu.el
+++ emacs/init_ubuntu.el
@@ -43,7 +43,7 @@
  '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
-    ("9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "c4e6fe8f5728a5d5fd0e92538f68c3b4e8b218bcfb5e07d8afff8731cc5f3df0" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "9824f5d6169b1e98c79e4a6f596b6a3f94cbefc0b2458fd2bfcbadee8b5687ec" default)))
+    ("2997ecd20f07b99259bddba648555335ffb7a7d908d8d3e6660ecbec415f6b95" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "c4e6fe8f5728a5d5fd0e92538f68c3b4e8b218bcfb5e07d8afff8731cc5f3df0" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "9824f5d6169b1e98c79e4a6f596b6a3f94cbefc0b2458fd2bfcbadee8b5687ec" default)))
  '(ediff-split-window-function (quote split-window-horizontally))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(erc-notifications-mode t)
@@ -54,7 +54,8 @@
     ("SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" ".ensime_cache")))
  '(package-selected-packages
    (quote
-    (markdown-mode magit js2-mode dockerfile-mode zenburn-theme wakatime-mode flycheck-haskell ensime use-package solarized-theme wanderlust w3m twittering-mode sx sublime-themes restclient pdf-tools paradox org-caldav multi-web-mode maker-mode magit-gh-pulls ipython hide-comnt haskell-mode gist fold-this ess-R-object-popup company-coq color-theme-zenburn color-theme-wombat color-theme-vim-insert-mode color-theme-twilight color-theme-tangotango color-theme-tango color-theme-solarized color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme-railscasts color-theme-monokai color-theme-molokai color-theme-library color-theme-ir-black color-theme-heroku color-theme-gruber-darker color-theme-github color-theme-emacs-revert-theme color-theme-eclipse color-theme-dpaste color-theme-dg color-theme-complexity color-theme-cobalt color-theme-buffer-local color-theme-approximate color-theme-actress boron-theme birds-of-paradise-plus-theme auto-package-update auto-complete auctex-latexmk)))
+    (yaml-mode intero markdown-mode magit js2-mode dockerfile-mode zenburn-theme wakatime-mode flycheck-haskell ensime use-package solarized-theme wanderlust w3m twittering-mode sx sublime-themes restclient pdf-tools paradox org-caldav multi-web-mode maker-mode magit-gh-pulls ipython hide-comnt haskell-mode gist fold-this ess-R-object-popup company-coq color-theme-zenburn color-theme-wombat color-theme-vim-insert-mode color-theme-twilight color-theme-tangotango color-theme-tango color-theme-solarized color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme-railscasts color-theme-monokai color-theme-molokai color-theme-library color-theme-ir-black color-theme-heroku color-theme-gruber-darker color-theme-github color-theme-emacs-revert-theme color-theme-eclipse color-theme-dpaste color-theme-dg color-theme-complexity color-theme-cobalt color-theme-buffer-local color-theme-approximate color-theme-actress boron-theme birds-of-paradise-plus-theme auto-package-update auto-complete auctex-latexmk)))
+ '(pdf-cache-image-limit 16)
  '(pdf-view-midnight-colors (quote ("gainsboro" . "gray15")))
  '(slack-buffer-create-on-notify (quote true))
  '(slack-buffer-emojify (quote true))
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,56 +1,33 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
-import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageDocks --  ( avoidStruts, docks )
 import XMonad.Util.Run
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Config.Desktop
 import XMonad.Util.SpawnOnce
 import XMonad.Actions.WorkspaceNames
---import XMonad.Actions.Volume
+-- import XMonad.Actions.Volume  -- xmonad-extras not compatible with xmonad 0.13
 import System.IO
-
--- baseConfig = desktopConfig
-
-
--- main = xmonad . myConfig =<< spawnPipe "xmobar"
-             
--- myConfig pipe = desktopConfig {modMask = controlMask .|. mod1Mask
---         , terminal = "rxvt"
---         , startupHook = startupHook baseConfig <+> spawnOnce "rxvt"
---         , logHook = myLogHook pipe
---         , manageHook = manageDocks
---         , layoutHook = avoidStruts $ layoutHook defaultConfig
---         } `additionalKeys`
---         [ ((controlMask .|. mod1Mask, xK_m), spawn "emacsclient -c"),
---           ((controlMask .|. mod1Mask, xK_s), spawn "systemctl suspend -i")--,
---  --         ((mod1Mask, xK_F7), lowerVolume 4 >> return()),
---  --         ((mod1Mask, xK_F8), raiseVolume 4 >> return())
-                                                                          
---         ]
-
--- myLogHook pipe = workspaceNamesPP xmobarPP {
---                    ppOutput = hPutStrLn pipe
---                  } >>= dynamicLogWithPP
-
 
 main :: IO ()
 main = do
   h <- spawnPipe "xmobar"
-  xmonad $ defaultConfig {
+  xmonad $ docks defaultConfig {
           modMask = controlMask .|. mod1Mask
         , XMonad.focusFollowsMouse = False
         , terminal = "gnome-terminal"
-        , startupHook = startupHook defaultConfig
+        -- , startupHook = docksStartupHook <+> startupHook defaultConfig
         , logHook = dynamicLogWithPP $ defaultPP {
                             ppOutput = hPutStrLn h
                                        }
-        , manageHook = manageDocks <+> manageHook defaultConfig
+        , manageHook = manageHook defaultConfig
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         } `additionalKeys`
         [ ((controlMask .|. mod1Mask, xK_m), spawn "emacsclient -c"),
           ((controlMask .|. mod1Mask, xK_s), spawn "systemctl suspend"),
           ((controlMask .|. mod1Mask, xK_n), spawn "nautilus -w"),
+          ((controlMask .|. mod1Mask, xK_f), spawn "firefox --new-window"),
           ((mod1Mask .|. shiftMask, xK_t), spawn "gnome-terminal")
  --         ((mod1Mask, xK_F7), lowerVolume 4 >> return()),
  --         ((mod1Mask, xK_F8), raiseVolume 4 >> return())
