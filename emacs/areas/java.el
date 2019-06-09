6ce95a9c6cb25d720d4f22402031383665df57d5
Peter Becich <peter@simspace.com>
2019-06-08 21:57:04 -0700

    gcmh

--- emacs/areas/java.el
+++ emacs/areas/java.el
@@ -24,13 +24,13 @@
 (add-hook 'java-mode-hook 'flycheck-mode)
 
 
-(require 'eclim)
-(setq eclimd-autostart t)
+;; (require 'eclim)
+;; (setq eclimd-autostart t)
 
-(defun my-java-mode-hook ()
-   (eclim-mode t))
+;; (defun my-java-mode-hook ()
+;;    (eclim-mode t))
 
-(add-hook 'java-mode-hook 'my-java-mode-hook)
+;; (add-hook 'java-mode-hook 'my-java-mode-hook)
 
 (require 'company-emacs-eclim)
 (company-emacs-eclim-setup)
