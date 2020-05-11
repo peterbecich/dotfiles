f45a7b262f25e392234558000ff23d9ce602e979
Peter Becich <peterbecich@gmail.com>
2020-05-10 20:47:06 -0700

    highlight color

--- emacs/init.el
+++ emacs/init.el
@@ -810,4 +810,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(linum ((t (:background "black" :foreground "#6c6c6c")))))
+ '(helm-selection ((t (:background "#90ee90"))))
+ '(linum ((t (:background "black" :foreground "#6c6c6c"))))
+ '(region ((t (:background "#87cefa")))))
