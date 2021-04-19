dbd44c69c106847d57356ebd6830b3524e63766a
Peter Becich <peterbecich@gmail.com>
2021-04-18 20:56:25 -0700

    lsp

--- emacs/areas/csharp.el
+++ emacs/areas/csharp.el
@@ -1,11 +1,4 @@
 
-(add-hook 'csharp-mode-hook 'omnisharp-mode)
-
-
-(eval-after-load
- 'company
- '(add-to-list 'company-backends 'company-omnisharp))
-
 (add-hook 'csharp-mode-hook #'company-mode)
 
 (add-hook 'csharp-mode-hook #'flycheck-mode)
